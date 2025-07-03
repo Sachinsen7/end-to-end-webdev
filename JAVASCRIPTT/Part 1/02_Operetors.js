ef215abc603dcdbdf8962384c8293c08a75e511c
Sachin Sen <sachinsen1920@gmail.com>
2025-07-03 10:11:34 +0530

    add JavaScript file for operators demonstration and examples

---
+++ JAVASCRIPTT/Part 1/02_Operetors.js
@@ -0,0 +1,81 @@
+let score = 100;
+let newScore = 100;
+
+let finalScore = score + newScore;
+
+console.log(finalScore);
+
+// Additions
+
+let addition = 10 + 10;
+console.log(addition);
+
+// Subtraction
+
+let subtraction = 10 - 10;
+console.log(subtraction);
+
+// Multiplication
+
+let multiplication = 10 * 10;
+console.log(multiplication);
+
+// Division
+
+let division = 10 / 10;
+console.log(division);
+
+// Modulus or Reminder
+
+let modulus = 10 % 10;
+console.log(modulus);
+
+// exponent
+
+let exponent = 10 ** 10;
+console.log(exponent);
+
+let myScore = 100;
+myScore++;
+++myScore;
+console.log(myScore);
+
+// Comparision Operators
+
+// (==)
+// (===)
+// (!=)
+// (!==)
+// (<)
+// (>)
+// (<=)
+// (>=)
+
+// // Logical Operators
+
+// (&&) => and
+// (||) => or
+// (!) => not
+
+let isLoggin = true;
+let isAdmin = false;
+
+console.log(isLoggin && isAdmin);
+console.log(isLoggin || isAdmin);
+console.log(!isLoggin);
+
+//Assignment Operators
+
+// (+=)
+// (-=)
+// (*=)
+// (/=)
+// (%=)
+// (**=)
+
+//Operators Precedence
+
+// (())
+
+let newscore = (5 * (6 + 2) - 2) / 7;
+console.log(newscore);
