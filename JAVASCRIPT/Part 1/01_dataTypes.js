99c0c646e861d431593fbe8cc63efa0dfafeaf22
Sachin Sen <sachinsen1920@gmail.com>
2025-07-07 22:09:10 +0530

    ......

---
+++ JAVASCRIPT/Part 1/01_dataTypes.js
@@ -0,0 +1,35 @@
+// First Program
+
+// console.log("My Name is Sachin");
+// process.stdout.write("My Name is Qaran");
+
+// DataTypes
+
+(String) => "Sachin";
+(Nmuber) => 100;
+(Boolean) => true;
+(BigInt) => 100n;
+(Symbol) => Symbol("hello");
+(Undefined) => undefined;
+(Null) => null;
+
+// Object => {name: "Sachin", age: 20,};
+(Object) => ["Sachin", "Shubham", "Rahul"];
+
+// Variables, Constants
+
+var score = 100; /* We Use Less Of These */
+let myScore = 102;
+const yourScore = 103; /* We Use Most Of These */
+
+let myName = "Sachin Kumar Sen";
+mySecondName = myName;
+
+console.log(mySecondName);
+
+// but in the const cases you can't do that
+
+const myScoree = 1000;
+myScoree = 2000;
+
+console.log(myScoree);
