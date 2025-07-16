c87c2a832537ee66395ca6ecded02bdc58f902cb
Sachin Sen <sachinsen1920@gmail.com>
2025-07-16 11:50:51 +0530

    import method

---
+++ JAVASCRIPTT/Part10_advance/appM.js
@@ -0,0 +1,7 @@
+// default import
+import multiply from "./mathOperationsM";
+
+//named import
+import { add, subtract } from "./mathOperationsM";
+console.log(add(4, 7));
+console.log(multiply(3, 4));
