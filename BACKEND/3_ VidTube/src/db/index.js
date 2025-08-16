4a9eba5289fd7578c07f99bfea972697758e262a
Sachin Sen <sachinsen1920@gmail.com>
2025-08-16 17:17:32 +0530

    update main file

--- BACKEND/3_ VidTube/src/db/index.js
+++ BACKEND/3_ VidTube/src/db/index.js
@@ -2,18 +2,18 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${DB_NAME}`
-    );
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URL}/${DB_NAME}`
+        );
 
-    console.log(
-      `\n MongoDB connected! DB host: ${connectionInstance.connection.host}`
-    );
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1);
-  }
+        console.log(
+            `\n MongoDB connected! DB host: ${connectionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
