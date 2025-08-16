69b4d695e018c6610f1a67a8848b8bb8106ddef5
Sachin Sen <sachinsen1920@gmail.com>
2025-08-16 17:17:18 +0530

    update health check controller

--- BACKEND/3_ VidTube/src/controllers/healthcheck.controllers.js
+++ BACKEND/3_ VidTube/src/controllers/healthcheck.controllers.js
@@ -2,9 +2,9 @@ import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-  return res
-    .status(200)
-    .json(new ApiResponse(200, "Ok", "Health check passed"));
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Ok", "Health check passed"));
 });
 
 export { healthcheck };
