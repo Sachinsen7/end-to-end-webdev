4b1aadf165859af6ad5eeed8bc69e469c9bdb320
Sachin Sen <sachinsen1920@gmail.com>
2025-08-17 12:15:53 +0530

    add login route

--- BACKEND/4_Vidtube/src/routes/users.routes.js
+++ BACKEND/4_Vidtube/src/routes/users.routes.js
@@ -1,22 +1,24 @@
-import { registerUser } from "../controllers/users.controllers.js";
+import { registerUser, loginUser } from "../controllers/users.controllers.js";
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router();
 
 router.post(
-  "/register",
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
-    {
-      name: "coverImage",
-      maxCount: 1,
-    },
-  ]),
-  registerUser
+    "/register",
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1,
+        },
+        {
+            name: "coverImage",
+            maxCount: 1,
+        },
+    ]),
+    registerUser
 );
+
+router.post("/login", loginUser);
 
 export default router;
