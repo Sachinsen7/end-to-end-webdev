ad097762f781861d22ca691426d631f97cde24b8
Sachin Sen <sachinsen1920@gmail.com>
2025-08-17 12:16:44 +0530

    update app file

--- BACKEND/4_Vidtube/src/app.js
+++ BACKEND/4_Vidtube/src/app.js
@@ -5,19 +5,19 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
 );
 
 app.use(express.json({ limit: "16kb" }));
 
 app.use(
-  express.urlencoded({
-    extended: true,
-    limit: "16kb",
-  })
+    express.urlencoded({
+        extended: true,
+        limit: "16kb",
+    })
 );
 
 app.use(cookieParser());
