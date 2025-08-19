57940a661eb3192ff9ecd6097ecaac7e400e88a4
Sachin Sen <sachinsen1920@gmail.com>
2025-08-19 12:08:49 +0530

    add playground authors

---
+++ MongoDb Advance/authors/playground-1.mongodb.js
@@ -0,0 +1,20 @@
+use("mongo-aggregation");
+
+// Create a new document in the collection.
+db.getCollection("authors").insertMany([
+  {
+    _id: 100,
+    name: "F. Scott Fitzgerald",
+    birth_year: 1896,
+  },
+  {
+    _id: 101,
+    name: "George Orwell",
+    birth_year: 1903,
+  },
+  {
+    _id: 102,
+    name: "Harper Lee",
+    birth_year: 1926,
+  },
+]);
