0428549e4b1f1e2475b46b65c08470e54c20e03b
Sachin Sen <sachinsen1920@gmail.com>
2025-08-20 12:21:09 +0530

    fix upload cloudinary

--- BACKEND/4_Vidtube/src/utils/cloudinary.js
+++ BACKEND/4_Vidtube/src/utils/cloudinary.js
@@ -6,54 +6,54 @@ import path from "path";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localPath) => {
-  try {
-    console.log("Cloudinary Config: ", {
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
-    });
+    try {
+        console.log("Cloudinary Config: ", {
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET,
+        });
 
-    if (!localPath) return null;
-    const normalizedPath = path.resolve(localPath).replace(/\\/g, "/");
-    console.log("Normalized Path: ", normalizedPath);
+        if (!localPath) return null;
+        const normalizedPath = path.resolve(localPath).replace(/\\/g, "/");
+        console.log("Normalized Path: ", normalizedPath);
 
-    const response = await cloudinary.uploader.upload(normalizedPath, {
-      resource_type: "auto",
-    });
+        const response = await cloudinary.uploader.upload(normalizedPath, {
+            resource_type: "auto",
+        });
 
-    console.log(
-      "File Uploaded in cloaudinary seuccessfully, File Path:",
-      response.url
-    );
+        console.log(
+            "File Uploaded in cloaudinary seuccessfully, File Path:",
+            response.url
+        );
 
-    // once the file is uploaded, we would like to delete it from the server
+        // once the file is uploaded, we would like to delete it from the server
 
-    fs.unlinkSync(localPath);
-    return response;
-  } catch (error) {
-    console.error("Cloudinary upload error:", error.message || error);
-    if (fs.existsSync(localPath)) fs.unlinkSync(localPath);
-    return null;
-  }
+        fs.unlinkSync(localPath);
+        return response;
+    } catch (error) {
+        console.error("Cloudinary upload error:", error.message || error);
+        if (fs.existsSync(localPath)) fs.unlinkSync(localPath);
+        return null;
+    }
 };
 
 const deleteFromCloudinary = async (publicId) => {
-  try {
-    const result = cloudinary.uploader.destroy(publicId);
-    console.log(
-      "file deleted from cloudinary successfully, File Path:",
-      publicId
-    );
-  } catch (error) {
-    comsole.error("error deleting file from cloudinary: ", error);
-    return null;
-  }
+    try {
+        const result = cloudinary.uploader.destroy(publicId);
+        console.log(
+            "file deleted from cloudinary successfully, File Path:",
+            publicId
+        );
+    } catch (error) {
+        comsole.error("error deleting file from cloudinary: ", error);
+        return null;
+    }
 };
 
 export { uploadOnCloudinary, deleteFromCloudinary };
