e57ee34b842c6ccc0df484a3f6bed5e7cbf958ea
Sachin Sen <sachinsen1920@gmail.com>
2025-08-21 20:01:20 +0530

    add ts file

---
+++ Typescript/arrays/basics.ts
@@ -0,0 +1,25 @@
+// const superHeros = []
+
+// superHeros.push("spiderman")   // gives the error
+
+// const superHeros: string[] = [];
+// const heroPower: number[] = [];
+
+heroPower.push(4);
+
+superHeros.push("spiderman");
+
+type Users = {
+  name: string;
+  isActive: boolean;
+};
+
+// push
+
+const allUser: Users[] = [];
+
+allUser.push({ name: "sachin", isActive: true });
+
+// one more way
+
+const Mlmodels: number[][] = [[255, 255, 255], []];
