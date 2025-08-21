f13ec47556696b599b9c5ed3a49eb9bb8b2fe68c
Sachin Sen <sachinsen1920@gmail.com>
2025-08-21 11:21:51 +0530

    add objects

---
+++ Typescript/objects/basics.ts
@@ -0,0 +1,23 @@
+const User = {
+  name: "Sachin",
+  email: "sachinsen@sen",
+  isActive: false,
+};
+
+function createUser({ name: string, isActive: boolean }) {}
+
+// createUser({ name: "sachin", isActive: true });
+
+let users = { name: "sachin", isActive: false, email: "sachin@sen" };
+
+createUser(users);
+
+// weired syntax
+
+function createCourse(): { name: string; price: number } {
+  return { name: "nextjs", price: 388 };
+}
+
+// Type alias
+
+export {};
