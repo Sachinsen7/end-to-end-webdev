462fb85cd9299bfa25432f7a101a9597c6a9e67a
Sachin Sen <sachinsen1920@gmail.com>
2025-08-21 20:03:47 +0530

    tuples in typescript

---
+++ Typescript/tuple/basics.ts
@@ -0,0 +1,13 @@
+// const user: (string | number)[] = [1, "se"];
+let newUser: [string, number, boolean];
+
+newUser = ["sachin", 121, true];
+
+let rgb: [number, number, number] = [323, 232, 434];
+
+type tUser = [number, string];
+
+const nwUser: tUser = [12123, "aHG"];
+
+nwUser[1] = "se";
+newUser.push(true);
