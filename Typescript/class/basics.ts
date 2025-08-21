3918fe7125da08c2fa28fe5743f33deb934eb439
Sachin Sen <sachinsen1920@gmail.com>
2025-08-21 20:02:28 +0530

    add class in ts

---
+++ Typescript/class/basics.ts
@@ -0,0 +1,14 @@
+// classes
+
+class Usern {
+  email: string;
+  name: string;
+  city: string = "";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
+
+const sachinnn = new User("se", "sachin");
+sachin.city = "Mumbai";
