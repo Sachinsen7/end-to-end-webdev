6559704e899d15bf68e5b56c9cc3f8088f5195e8
Sachin Sen <sachinsen1920@gmail.com>
2025-08-21 20:01:41 +0530

    add variables in ts

---
+++ Typescript/basics/variables.ts
@@ -0,0 +1,28 @@
+let greetings: string = "Hello World!";
+
+console.log(greetings);
+greetings.toLowerCase();
+
+let myNum = 6;
+myNum.toPrecision(2);
+
+// number
+
+let userId: number = 247354;
+
+userId.toPrecision(3);
+
+// boolean
+
+let isDone: boolean = true;
+isDone = false;
+
+// any
+
+let hero: string;
+
+function getHero() {
+  return "thor";
+}
+
+hero = getHero();
