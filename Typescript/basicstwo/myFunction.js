e276aa83eb5c2e42bc8b8cac3ebf5377288bf7eb
Sachin Sen <sachinsen1920@gmail.com>
2025-08-21 20:02:11 +0530

    add functions in ts

---
+++ Typescript/basicstwo/myFunction.js
@@ -0,0 +1,19 @@
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(6);
+// fixes
+function addTwoo(num) {
+    return num + 2;
+}
+// string
+function getUpper(val) {
+    return val.toUpperCase();
+}
+getUpper("hello");
+// fixes
+function signUpUser(name, email, isPaid) { }
+signUpUser("sachin", "sachin@123", true);
+// arrow function
+var loginUser = function (email, password, isPaid) { };
+loginUser("sachin", "sachin@123", true);
