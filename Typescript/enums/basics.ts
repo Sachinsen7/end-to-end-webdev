688e56e91afae8d8e8e08b51820b54ed0fe2655f
Sachin Sen <sachinsen1920@gmail.com>
2025-08-21 20:02:38 +0530

    add enums

---
+++ Typescript/enums/basics.ts
@@ -0,0 +1,12 @@
+const AISLE = 0;
+const MIDDLE = 1;
+const WINDOW = 2;
+
+enum SeatChoice {
+  AISLE = 10,
+  MIDDLE = 22,
+  WINDOW = "Window",
+  FOURTH = "se",
+}
+
+const seSeat = SeatChoice.FOURTH;
