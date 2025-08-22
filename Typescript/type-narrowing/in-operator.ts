09d93ae8d16b0c194ba7670833242549220c0a46
Sachin Sen <sachinsen1920@gmail.com>
2025-08-22 17:20:06 +0530

    the in operator in typescript

---
+++ Typescript/type-narrowing/in-operator.ts
@@ -0,0 +1,18 @@
+// the in operator narrowing
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface Admin {
+  name: string;
+  email: string;
+  isAdmin: boolean;
+}
+
+function isAdminAccount(account: User | Admin) {
+  if ("isAdmin" in account) {
+    return account.isAdmin;
+  }
+}
