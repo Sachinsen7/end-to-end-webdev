3575efd4c1518ac58ec72847c15f1f654ab4d30f
Sachin Sen <sachinsen1920@gmail.com>
2025-08-24 01:49:35 +0530

    add routes function s

---
+++ NextJS/new-one/my-app/src/app/api/users/signup/route.ts
@@ -0,0 +1,48 @@
+import { connectDB } from "@/dbConfig/db";
+import UserModel from "@/model/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+    console.log(reqBody);
+
+    // checkpoint
+
+    const user = await UserModel.findOne({ email });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // save user
+
+    const newUser = await UserModel.create({
+      username: username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+
+    return NextResponse.json({
+      message: "User created successfully",
+      success: true,
+      user: newUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
