360a87b4815024e998af9273ec0eddb2656e27be
Sachin Sen <sachinsen1920@gmail.com>
2025-08-23 21:44:55 +0530

    add signup

---
+++ NextJS/new-one/my-app/src/app/signup/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignUp = async () => {};
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        type="text"
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+
+      <label htmlFor="email">email</label>
+      <input
+        type="text"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+
+      <label htmlFor="password">password</label>
+      <input
+        type="text"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+
+      <button onClick={onSignUp}>Sign up</button>
+      <Link href="/login">
+        Already have an account <span className="underline">login here</span>
+      </Link>
+    </div>
+  );
+}
