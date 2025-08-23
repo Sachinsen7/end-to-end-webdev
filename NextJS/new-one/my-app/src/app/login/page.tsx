34fb4fee49b5817e6e1b5af2412f1da504a69181
Sachin Sen <sachinsen1920@gmail.com>
2025-08-24 02:06:40 +0530

    fetch login api

--- NextJS/new-one/my-app/src/app/login/page.tsx
+++ NextJS/new-one/my-app/src/app/login/page.tsx
@@ -1,20 +1,46 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
 
-  const onLogin = async () => {};
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("login success", response.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("login failed", error);
+      toast.error("Login failed");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div>
-      <h1>Login</h1>
+      <h1>{loading ? "Processing" : "Login"}</h1>
       <hr />
 
       <label htmlFor="email">email</label>
@@ -35,7 +61,7 @@ export default function LoginPage() {
         placeholder="password"
       />
 
-      <button onClick={onLogin}>Login</button>
+      <button onClick={onLogin}>{buttonDisabled ? "No Login" : "Login"}</button>
       <Link href="/signup">
         Don't have an account <span className="underline">Signup here</span>
       </Link>
