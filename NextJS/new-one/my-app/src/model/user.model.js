f706dd65f57f62b2dbd7904763d52ac8233270e9
Sachin Sen <sachinsen1920@gmail.com>
2025-08-24 01:19:18 +0530

    add user model

---
+++ NextJS/new-one/my-app/src/model/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+const UserModel = mongoose.models.users || mongoose.model("Users", userSchema);
+
+export default UserModel;
