57298fb920bbf5486c057436e31219bc713a58b6
Sachin Sen <sachinsen1920@gmail.com>
2025-08-24 02:06:07 +0530

    add login api

---
+++ NextJS/new-one/my-app/src/app/api/users/login/route.ts
@@ -0,0 +1,59 @@
+import { connectDB } from "@/dbConfig/db";
+import UserModel from "@/model/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+    console.log(reqBody);
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exists" },
+        { status: 400 }
+      );
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+    }
+
+    // create tokenData
+
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json(
+      {
+        message: "User logged in successfully",
+        success: true,
+      },
+      { status: 200 }
+    );
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (error: any) {
+    console.log("login error", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
