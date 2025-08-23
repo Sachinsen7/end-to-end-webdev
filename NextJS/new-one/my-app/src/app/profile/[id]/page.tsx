3b896f4f2a6127cc6ca2e56a0ae1e033a81c6dbd
Sachin Sen <sachinsen1920@gmail.com>
2025-08-23 21:52:14 +0530

    update params

--- NextJS/new-one/my-app/src/app/profile/[id]/page.tsx
+++ NextJS/new-one/my-app/src/app/profile/[id]/page.tsx
@@ -1,10 +1,11 @@
-export default function UserProfile({ params }: any) {
+export default async function UserProfile({ params }: any) {
+  const { id } = params;
   return (
     <div>
       <h1>Profile</h1>
       <hr />
       <p>
-        Profile <span>{params.id}</span>
+        Profile <span>{id}</span>
       </p>
     </div>
   );
