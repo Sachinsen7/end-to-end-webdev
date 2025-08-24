d61ec0a56059bf36167449f1c40bec75be5724a5
Sachin Sen <sachinsen1920@gmail.com>
2025-08-24 16:17:17 +0530

    update verify email

---
+++ NextJS/new-one/my-app/src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,31 @@
+import { connectDB } from "@/dbConfig/db";
+import { NextRequest, NextResponse } from "next/server";
+import UserModel from "@/model/user.model";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
+    const user = await UserModel.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+    }
+
+    console.log(user);
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+    await user.save();
+
+    return NextResponse.json({ message: "Email verified", success: true });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
