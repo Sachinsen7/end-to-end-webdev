64800854d7f1534619b21dfcce069b3462039be5
Sachin Sen <sachinsen1920@gmail.com>
2025-08-28 23:07:59 +0530

    update subscripton model

--- BACKEND/4_Vidtube/src/models/suscription.model.js
+++ BACKEND/4_Vidtube/src/models/suscription.model.js
@@ -1,17 +1,18 @@
 import mongoose from "mongoose";
 
 const subscriptionSchema = new mongoose.Schema(
-  {
-    subscriber: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+    {
+        subscriber: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        channel: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
     },
-    channel: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
-export default mongoose.model("Subscription", subscriptionSchema);
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+export default Subscription;
