94e238a58428d89b54440ce2291f8069fec92e8b
Sachin Sen <sachinsen1920@gmail.com>
2025-08-29 21:40:26 +0530

    add dashboard route

--- BACKEND/4_Vidtube/src/routes/dashboard.routes.js
+++ BACKEND/4_Vidtube/src/routes/dashboard.routes.js
@@ -1,17 +1,15 @@
-import mongoose from "mongoose";
-import { Video } from "../models/video.model.js";
-import { Subscription } from "../models/subscription.model.js";
-import { Like } from "../models/like.model.js";
-import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { Router } from "express";
+import {
+    getChannelStats,
+    getChannelVideos,
+} from "../controllers/dashboard.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-});
+const router = Router();
 
-const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-});
+router.use(verifyJWT);
 
-export { getChannelStats, getChannelVideos };
+router.route("/stats").get(getChannelStats);
+router.route("/videos").get(getChannelVideos);
+
+export default router;
