e221b271c6a05701155a56271d66d4ee930ce00b
Sachin Sen <sachinsen1920@gmail.com>
2025-09-03 20:03:24 +0530

    update route

--- BACKEND/4_Vidtube/src/routes/tweet.routes.js
+++ BACKEND/4_Vidtube/src/routes/tweet.routes.js
@@ -10,7 +10,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/").post(createTweet);
+router.route("/tweet").post(createTweet);
 router.route("/user/:userId").get(getUserTweets);
 router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
 
