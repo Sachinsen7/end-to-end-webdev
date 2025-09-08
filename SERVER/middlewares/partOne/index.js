cae381664528376d4115d94643f8bd9bf5f37b49
Sachin Sen <sachinsen1920@gmail.com>
2025-09-08 23:08:12 +0530

    add index

--- SERVER/middlewares/partOne/index.js
+++ SERVER/middlewares/partOne/index.js
@@ -1,7 +1,6 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
-
+const app = express();
 
 // app.get("/sum", (req, res) => {
 //     const a = parseInt(req.query.a)
@@ -12,7 +11,7 @@ const app = express()
 //     })
 // })
 
-// //dynamic params query 
+// //dynamic params query
 
 // app.get("/sum/:firstArg/:secondArg", (req, res) => {
 //     const a = parseInt(req.params.firstArg)
@@ -23,31 +22,25 @@ const app = express()
 //     })
 // })
 
-
 //middlewares
 
 let reqCount = 0;
 
-function Increase(req, res, next){
-    reqCount = reqCount + 1;
-    // if(req.headers.cookie === "goggle"){
-    //     next()
-    // } else {
-    //     console.log("total no. of request", reqCount)  // it can stop the requests 
-    //     res.json({
-    //         message: "I ended the request early"
-    //     })
-    // }
+function Increase(req, res, next) {
+  reqCount = reqCount + 1;
+  // if(req.headers.cookie === "goggle"){
+  //     next()
+  // } else {
+  //     console.log("total no. of request", reqCount)  // it can stop the requests
+  //     res.json({
+  //         message: "I ended the request early"
+  //     })
+  // }
 
-    res.json({
-        message: "I ended the request early"
-    })
-
-  
-   
+  res.json({
+    message: "I ended the request early",
+  });
 }
-    
-
 
 // app.get("/sub", (req, res) => {
 //     Increase()
@@ -59,48 +52,44 @@ function Increase(req, res, next){
 //     })
 // })
 
-function realSumHandler(req, res){
-    const a = req.query.a
-    const b = req.query.b
-    
-    res.json({
-        answer : a - b
-    })
+function realSumHandler(req, res) {
+  const a = req.query.a;
+  const b = req.query.b;
+
+  res.json({
+    answer: a - b,
+  });
 }
 
 // you can do this too
-app.use(Increase)  // this is middleware
+app.use(Increase); // this is middleware
 
-app.get("/sum",  realSumHandler) // shoter wayyy
-app.get("/multiply",  realSumHandler) // shoter wayyy
-app.get("/divide",  realSumHandler) // shoter wayyy
+app.get("/sum", realSumHandler); // shoter wayyy
+app.get("/multiply", realSumHandler); // shoter wayyy
+app.get("/divide", realSumHandler); // shoter wayyy
 
-
-app.get("/sum", Increase, realSumHandler) // shoter wayyy
-app.get("/multiply", Increase, realSumHandler) // shoter wayyy
-app.get("/divide", Increase, realSumHandler) // shoter wayyy
-
-
+app.get("/sum", Increase, realSumHandler); // shoter wayyy
+app.get("/multiply", Increase, realSumHandler); // shoter wayyy
+app.get("/divide", Increase, realSumHandler); // shoter wayyy
 
 app.get("/sub", Increase, (req, res) => {
-    Increase()
-    const a = req.query.a
-    const b = req.query.b
+  Increase();
+  const a = req.query.a;
+  const b = req.query.b;
 
-    res.json({
-        answer : a - b
-    })
-})  
-
+  res.json({
+    answer: a - b,
+  });
+});
 
 app.use("/divide/:a/:b", (req, res) => {
-    Increase()
-    const a = req.params.a
-    const b = req.params.b
+  Increase();
+  const a = req.params.a;
+  const b = req.params.b;
 
-    res.json({
-        answer: a / b
-    })
-})
+  res.json({
+    answer: a / b,
+  });
+});
 
-app.listen(3000)
+app.listen(3000);
