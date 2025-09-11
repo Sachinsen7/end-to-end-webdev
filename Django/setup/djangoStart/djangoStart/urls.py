8d7ff1a64416e8e7a5eb64ed02406c1f046cca39
Sachin Sen <sachinsen1920@gmail.com>
2025-09-11 18:53:47 +0530

    add urls

--- Django/setup/djangoStart/djangoStart/urls.py
+++ Django/setup/djangoStart/djangoStart/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.home, name='home'),
+    path("about/", views.contact, name='about'),
+    path("contact/", views.about, name='contact'),
+
 ]
