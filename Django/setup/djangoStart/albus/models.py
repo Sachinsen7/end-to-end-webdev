1dc111e638464bbb760ee68d2b9373458874c9ee
Sachin Sen <sachinsen1920@gmail.com>
2025-09-13 19:02:57 +0530

    add tea models

--- Django/setup/djangoStart/albus/models.py
+++ Django/setup/djangoStart/albus/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ChaiVarity(models.Model):
+    CHAI_TYPE_CHOICE = [
+        ('Tea', 'Tea'),
+        ('Coffee', 'Coffee'),
+        ('Milk', 'Milk'),
+    ]
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images')
+    date_added = models.DateTimeField(default=timezone.now)
+    description = models.TextField(default='')
+
+
+    type = models.CharField(max_length=20, choices=CHAI_TYPE_CHOICE)
+
+
+    def __str__(self):
+        return self.name
+
+
