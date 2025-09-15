cdaf0a294e6aee83ddc40e7d3d7150c86bca4847
Sachin Sen <sachinsen1920@gmail.com>
2025-09-15 22:00:30 +0530

    add admin methods

--- Django/setup/djangoStart/albus/admin.py
+++ Django/setup/djangoStart/albus/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
-from .models import ChaiVarity
+from .models import ChaiVarity, TeaReviews, TeaCertificate, Store
+
+
 
 # Register your models here.
 
+class TeaReviewInline(admin.TabularInline):
+    model = TeaReviews
+    extra = 2
 
-admin.site.register(ChaiVarity)
+class TeaVarietyAdmin(admin.ModelAdmin):
+    list_display = {'name', 'type', 'date_added',}
+    inlines = [TeaReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = {'name', 'location'}
+    filter_horizontal = ('chai_variety',)
+
+class TeaCertificateAdmin(admin.ModelAdmin):
+    list_display = {'tea', 'certificate_number',}
+
+
+admin.site.register(Store, StoreAdmin)
+admin.site.register(TeaReviews)
+admin.site.register(TeaCertificate)
+
+admin.site.register(ChaiVarity, TeaVarietyAdmin)
