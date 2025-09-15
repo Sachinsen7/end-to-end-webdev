428a8ec58d695d3dc61d39a0516c2190036c9ea5
Sachin Sen <sachinsen1920@gmail.com>
2025-09-15 21:14:37 +0530

    add two sum java

---
+++ DSA/DSA JAVA/assignments/AddTwoSum.java
@@ -0,0 +1,31 @@
+public class AddTwoSum {
+    public static void main(String[] args){
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        Solution solution = new Solution();
+        int[] result = solution.twoSum(nums, target);
+
+        if(result.length == 2){
+            System.out.println("Indices: " + result[0] + " " + result[1]);
+        } else{
+            System.out.println("No two numbers found");
+        }
+
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target){
+        int n = nums.length;
+        for(int i = 0; i < n - 1; i++){
+            for (int j = i + 1; j < n; j++) {
+                if((nums[i] + nums[j]) == target){
+                    return new int[]{i, j};
+                }
+            }
+
+        }
+        return new int[]{};
+    }
+}
